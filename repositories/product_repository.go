4d2b88d38996eee2b46b15d27d5ab1a8c136e460
ginaa07 <1123150124@global.ac.id>
2026-04-10 01:16:37 +0700

    add type productrepositories struct

--- repositories/product_repository.go
+++ repositories/product_repository.go
@@ -4,3 +4,8 @@ import (
 	"github.com/ReginaSafarina/gin-firebase-backend/config"
 	"github.com/ReginaSafarina/gin-firebase-backend/models"
 )
+
+type ProductRepository struct{}
+func NewProductRepository() *ProductRepository {
+	return &ProductRepository{}
+}
