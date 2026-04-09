7e63021e5993b6bbfdef6635b48b420796a86f71
ginaa07 <1123150124@global.ac.id>
2026-04-09 22:06:33 +0700

    rename github

--- config/database.go
+++ config/database.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"github.com/yourusername/gin-firebase-backend/models"
+	"github.com/ReginaSafarina/gin-firebase-backend/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
