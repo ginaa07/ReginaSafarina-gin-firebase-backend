ef255c6fe21f051364482b657eaea7a176ff5851
ginaa07 <1123150124@global.ac.id>
2026-04-10 01:30:59 +0700

    VerifyFirebaseToken

--- services/auth_service.go
+++ services/auth_service.go
@@ -13,3 +13,22 @@ import (
 	"github.com/ReginaSafarina/gin-firebase-backend/repositories"
 	"gorm.io/gorm"
 )
+
+type AuthService struct {
+	userRepo *repositories.UserRepository
+}
+
+func NewAuthService() *AuthService {
+	return &AuthService{userRepo: repositories.NewUserRepository()}
+}
+
+func (s *AuthService) VerifyFirebaseToken(firebaseToken string) (string, *models.User, error){
+	token, err := config.FirebaseAuth.VerifyIDToken(context.Background(), firebaseToken)
+	if err != nil {
+		return "", nil, errors.New("firebase token tidak valid atau kadaluarsa")
+	}
+
+	emailVerified, _ := token.Claims["email_verified"].(bool)
+	if !emailVerified {
+		return "", nil, errors.New("EMAIL_NOT_VERIFIED")
+	}
