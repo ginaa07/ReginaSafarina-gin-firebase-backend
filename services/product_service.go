b4136f7541e761d94ab92b32c49a9761ffd607de
ginaa07 <1123150124@global.ac.id>
2026-04-10 15:18:52 +0700

    feat: func productService

--- services/product_service.go
+++ services/product_service.go
@@ -12,3 +12,17 @@ type ProductService struct {
 func NewProductService() *ProductService {
 	return &ProductService{productRepo: repositories.NewProductRepository()}
 }
+
+func (s *ProductService) GetAll(page, limit int, category string) ([]models.Product, int64, error) {
+		if page <= 0 { 
+			page = 1 
+		}
+		if limit <= 0 || limit > 100 { 
+			limit = 10 
+		}
+		return s.productRepo.FindAll(page, limit, category)
+}
+
+func (s *ProductService) GetByID(id uint) (*models.Product, error){
+	return s.productRepo.FindByID(id)
+}
