c6520eca2f4c652a46f3f2a2baa972e949d8595e
ginaa07 <1123150124@global.ac.id>
2026-04-10 15:39:40 +0700

    feat: func AuthMiddleware
    Authorization

--- middleware/auth_middleware.go
+++ middleware/auth_middleware.go
@@ -8,3 +8,25 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 )
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func (c *gin.Context){
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"success": false,
+				"message": "Authorization header tidak ditemukan",
+				"error_code": "MISSING_TOKEN",
+			})
+			return
+		}
+
+		parts := strings.SplitN(authHeader, " ", 2)
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"succcess": false,
+				"message": "format tokeb salah. gunakan: Bearer <token>",
+				"error_code": "INVALID_TOKEN_FORMAT",
+			})
+			return
+		}
